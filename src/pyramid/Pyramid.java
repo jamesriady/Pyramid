0b21c2eaeeb5e3c53b5754a434e0f82278bceaca
james <jamesriady1998@yahoo.com>
2016-09-20 14:59:14 +0700

    java pyramid(revisi)

--- src/pyramid/Pyramid.java
+++ src/pyramid/Pyramid.java
@@ -11,23 +11,30 @@ package pyramid;
  */
 public class Pyramid {
     public static void main(String args[]) {
-		for(int i=1;i<25;i++) {
-			for(int j=1;j<=i ;j++) {
+		int i,j,k;
+		int n=25;
+		for(i = 0;i<n;i++) {
+			if(i%2 != 1) {
+			for(j=0;j<i;j++) {
+				if(j%2 != 1){
 				System.out.print(" ");
+			}}
+			for(k=0;k<n-i;k++){
+				System.out.print("*");
 			}
-			for(int k=0;k<=25-i;k++) {
-				System.out.print("* ");
-			}	
-		System.out.println("");
+			}
+			System.out.println("");
 		}
-		for(int i=0;i<25;i++) {
-			for(int j=0;j<25-i ;j++) {
+		for(i = 1;i<n;i++) {
+			if(i%2 !=1) {
+			for(j=1;j<n-i;j++) {
+				if(j%2 != 1) {
 				System.out.print(" ");
-			}
-			for(int k=0;k<=i;k++) {
-				System.out.print("* ");
-			}
-		System.out.println("");
+			}}
+			for(k=0;k<=i;k++){
+				System.out.print("*");
+			}}
+			System.out.println("");
 		}
     }
 }
